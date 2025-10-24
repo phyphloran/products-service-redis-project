489be97a7d00705578b6e5f2e06b69f2018dbdf7
phyphloran <mareeviv@yandex.ru>
2025-10-24 18:16:51 +0300

    initialization

---
+++ src/main/java/ProductsProject/ProductsProject/ProductsProjectApplication.java
@@ -0,0 +1,13 @@
+package ProductsProject.ProductsProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ProductsProject/ProductsProject/ProductsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ProductsProject.ProductsProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
